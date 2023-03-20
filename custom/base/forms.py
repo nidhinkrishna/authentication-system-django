354e14f4c5dbeee56142eec930dd40c2ab5e5df6
nidhinkrishna <nidhingopalank@gmail.com>
2023-03-20 20:27:24 +0530

    styling of login and signup page completed

--- custom/base/forms.py
+++ custom/base/forms.py
@@ -4,4 +4,4 @@ from django.contrib.auth.forms import UserCreationForm
 class SignUpForm(UserCreationForm):
     class Meta:
         model=CustomUser
-        fields=['name','email','password1','password2']
+        fields=['email','name','password1','password2']
